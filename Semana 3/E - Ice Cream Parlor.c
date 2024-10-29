c2856408fc2ac77b8df962c2a1022233e3db2678
Rafael Medeiros <163771494+leitaonerd@users.noreply.github.com>
2024-10-29 15:40:03 -0300

    semana 3.3

--- Semana 3/E - Ice Cream Parlor.c
+++ Semana 3/E - Ice Cream Parlor.c
@@ -5,46 +5,23 @@ void Troca(int *a, int *b);
 int main(void) {
 
   int t, k, n;
-  
+
   scanf("%d", &t);
   for(int i = 0; i < t; i++){
     scanf("%d", &k);
     scanf("%d", &n);
     getchar();
-    
-    int cost[n], cpy[n];
+
+    int cost[n];
     for(int j = 0; j < n; j++){
       scanf("%d", &cost[j]);
-      cpy[j] = cost[j];
     }
     for(int j = 0; j < n-1; j++){
-      for(int k = j+1; k < n; k++){
-        if(cost[j] > cost[k]) Troca(&cost[j], &cost[j]);
+      for(int l = j+1; l < n; l++){
+        if(cost[j] + cost[l] == k) printf("%d %d\n", j+1, l+1);
       }
-    }
-  
-    int a = 0, b = n-1;
-    while((cost[a]+cost[b]) != k){
-      if(cost[a]+cost[b] > k) b--;
-      else if(cost[a]+cost[b] < k) a++;
-    }
-    
-    a = cost[a];
-    b = cost[b];
-
-    for(int j = 0; j < n; j++){
-      if(cpy[j] == a) printf("%d ", j+1);
-    }
-    for(int j = 0; j < n; j++){
-      if(cpy[j] == b) printf("%d\n", j+1);
     }
   }
 
   return 0;
-}
-
-void Troca(int *a, int *b){
-  int temp = *a;
-  *a = *b;
-  *b = temp;
 }
