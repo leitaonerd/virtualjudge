3090298d432f4db35e65cf811233e1752d7741be
leitaonerd <leitao.rma@gmail.com>
2024-11-15 15:57:26 -0300

    semana 5.1

---
+++ Semana 5/F - Desafio - Prisoes e Wynverns.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define MOD 1000000007;
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    int dano[n];
+    dano[0] = 1;
+    
+    for(int i = 1; i <= n; i++){
+        dano[i] = 0;
+        for(int j = 1; j <= 6; j++){
+            if(i - j >= 0) dano[i] = (dano[i] + dano[i-j]) % MOD;
+        }
+    }
+
+    printf("%d\n", dano[n]);
+
+    return 0;
+}
