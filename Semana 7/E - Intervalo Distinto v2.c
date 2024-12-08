155cda4b038a77df12a6615afe684b81a6507d80
leitaonerd <leitao.rma@gmail.com>
2024-12-08 12:25:40 -0300

    semana 7.4

--- Semana 7/E - Intervalo Distinto v2.c
+++ Semana 7/E - Intervalo Distinto v2.c
@@ -1,39 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(){
-
-    int n, head, atualcont = 0, maxcont = 0, hashmap[100000] = {0};
-
+int main() {
+    int n, head = 0, atualcont = 0, maxcont = 0, hashmap[100000] = {0};
     scanf("%d", &n);
-    int array[n];
 
-    for(int i = 0; i < n; i++){
+    int array[n];
+    for (int i = 0; i < n; i++) {
         scanf("%d", &array[i]);
     }
 
-    head = 0;
     for(int i = 0; i < n; i++){
-        if(hashmap[array[i]] == 0){
-            hashmap[array[i]]++;
-            atualcont++;
-        }
-        else{
-            int j = head;
-            while(array[j] != array[i]){
-                j++;
-            }
-            head = j++;
+        hashmap[array[i]]++;
 
-            if(atualcont > maxcont) maxcont = atualcont;
-            memset(hashmap, 0, 100000* sizeof(int));
-            atualcont = 0;
-            i = head;
+        while(hashmap[array[i]] > 1){
+            hashmap[array[head]]--;
+            head++;
+        }
+
+        atualcont = i - head + 1;
+        if(atualcont > maxcont){
+            maxcont = atualcont;
         }
     }
-
-    if(atualcont > maxcont) maxcont = atualcont;
 
     printf("%d\n", maxcont);
 
--- Semana 7/E - Intervalo Distinto.c
+++ Semana 7/E - Intervalo Distinto.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-//adicionar hashmap JUNTO da linked list
-struct Node{
-    int data;
-    struct Node* next;
-};
-
-struct Node* newNode(int data){
-    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-
-    temp -> data = data;
-    temp -> next = NULL;
-    return temp;
-}
+#include <string.h>
 
 int main(){
 
-    int n, a, cont, maxcont = 0;
-    struct Node* head = NULL;
-    struct Node* curr = NULL;
+    int n, head, atualcont = 0, maxcont = 0, hashmap[100000] = {0};
 
     scanf("%d", &n);
+    int array[n];
 
     for(int i = 0; i < n; i++){
-        scanf("%d", &a);
-        struct Node* temp = newNode(a);
-
-        if(head == NULL) head = temp;
-        else curr -> next = temp;
-
-        curr = temp;
+        scanf("%d", &array[i]);
     }
 
+    head = 0;
+    for(int i = 0; i < n; i++){
+        if(hashmap[array[i]] == 0){
+            hashmap[array[i]]++;
+            atualcont++;
+        }
+        else{
+            int j = head;
+            while(array[j] != array[i]){
+                j++;
+            }
+            head = j++;
 
+            if(atualcont > maxcont) maxcont = atualcont;
+            memset(hashmap, 0, 100000* sizeof(int));
+            atualcont = 0;
+            i = head;
+        }
+    }
 
+    if(atualcont > maxcont) maxcont = atualcont;
+
+    printf("%d\n", maxcont);
 
     return 0;
 }
